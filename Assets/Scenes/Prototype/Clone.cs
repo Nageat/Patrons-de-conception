e8e72b0e26bd09503f2e2debad3c5eb6392596b5
Galati Gaetan <skyfulle68@gmail.com>
2020-11-02 15:16:16 +0100

    Clone

--- Assets/Scenes/Prototype/Clone.cs
+++ Assets/Scenes/Prototype/Clone.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Clone : MonoBehaviour
 {
-    public voiture Voiture;
+    [SerializeField]
+    private voiture Voiture;
     
     public virtual void Vitesse()
     {
