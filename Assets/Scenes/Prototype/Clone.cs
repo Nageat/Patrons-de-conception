85a13174c37e120ecb023d859f6ef08e33798e29
Galati Gaetan <skyfulle68@gmail.com>
2020-11-02 15:13:57 +0100

    Clone
    Ne marche pas

---
+++ Assets/Scenes/Prototype/Clone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clone : MonoBehaviour
+{
+    public voiture Voiture;
+    
+    public virtual void Vitesse()
+    {
+        Debug.Log(Voiture.Couleur + "Roule a " + Voiture.speed);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
