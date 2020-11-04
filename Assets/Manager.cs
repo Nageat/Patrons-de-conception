dae37e17375394e504f152de9ae668cde6137b4a
Galati Gaetan <skyfulle68@gmail.com>
2020-11-04 11:44:44 +0100

    Singleton

---
+++ Assets/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField]
+    public static Manager instance = null;//Class manager
+    [SerializeField]
+    public int HP = 3;//PV -> HealthScript
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;//Si instance est null assigne a ce gameobject
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);//Sinon la détruire
+        }
+
+    }
+}
